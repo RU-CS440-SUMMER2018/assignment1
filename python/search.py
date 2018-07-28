56022d4041cf9f684df2f88e38ac1781a8e1b0c2
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2018-07-28 02:41:50 -0400

    read maze from file function

--- python/search.py
+++ python/search.py
@@ -33,3 +33,34 @@ def search(filename, initial_i, initial_j, goal_i, goal_j):
         retList.append((goal_i, j))
 
     return retList
+
+# Returns a 2D list[i][j] representing
+# the maze in filename
+def readMaze(filename):
+
+    # Read file
+    file = open(filename, 'r')
+    lines = file.readlines()
+    file.close()
+    
+    # Get rows and columns
+    rows = int(lines[0])
+    cols = int(lines[1])
+    del lines[0]
+    del lines[0]
+
+    # Initialize the return list
+    retList = []
+    for i in range(cols):
+        retList.append([])
+    
+    # Save integers to return list
+    col = 0
+    while col < cols:
+        row = 0
+        while row < rows:
+            retList[col].append(int(lines[col][row*2]))
+            row += 1
+        col += 1
+
+    return retList
