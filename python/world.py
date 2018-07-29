d48593b8d8fd82ce2b2784be001a8052d0c79d45
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2018-07-29 02:49:58 -0400

    commented

--- python/world.py
+++ python/world.py
@@ -1,6 +1,7 @@
 import astar
 
 class State:
+    'Represents a state in this envioronment'
     def __init__(self, i, j):
         self.i = i
         self.j = j
@@ -8,18 +9,31 @@ class State:
         return (self.i == other.i) and (self.j == other.j)
 
 def manhattanHeuristic(fromNode, toNode):
+    '''
+    Hueristic that measures the manhattan distence from
+    <fromNode> to <toNode> and returns it
+    '''
     return abs(toNode.state.i - fromNode.state.i) + abs(toNode.state.j - fromNode.state.j)
 
 def zeroHeuristic(fromNode, toNode):
+    'This hueristic always returns 0'
     return 0
 
 def findActions(node, environment):
+    '''
+    Finds all the actions <node> can make
+    in <envoronment> and returns them in
+    a list
+    '''
 
     actions = []
     i = node.state.i
     j = node.state.j
 
     for ij in [(i+1,j),(i-1,j),(i,j+1),(i,j-1)]:
+
+        # Save actions that lead you to nodes that are within
+        # the maze and are not blocked (a 0)
         try: 
             if (not ij[0] < 0) and (not ij[1] < 0) and environment[ij[0]][ij[1]]:
                 neighborNode = astar.Node(State(ij[0], ij[1]), manhattanHeuristic)
