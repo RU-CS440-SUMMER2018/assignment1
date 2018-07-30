5735efc3fcfb3b9ee2bc8e32f2ef14b0a08bf37f
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2018-07-30 17:39:27 -0400

    explained configurations

--- python/config.py
+++ python/config.py
@@ -1,4 +1,12 @@
 import world
 
+# Set this variable to a heuristic function of your choice.
+# A heuristic function must follow: (fromNode, toNode) => int.
+# Available heuristic functions are: world.manhattanHeuristic
+# and world.zeroHeuristic.
 heuristicFunction = world.manhattanHeuristic
+
+# Set this variable to give the weight to the
+# hValue over the gValue when calculating the
+# fValue
 heuristicWeight = 1
